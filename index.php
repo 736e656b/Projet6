edc1c7eff31d2be9c57245ddef2abc2f101dc5df
the_nt <25741009+the-nt@users.noreply.github.com>
2019-12-13 09:47:31 +0100

    table

--- index.php
+++ index.php
@@ -17,15 +17,22 @@
         <div class="bloc1">
             <div class="table">
                 <a href="traitement.php?plat=test1&prix=1.1">
-                    <img class="plat" src="resources/images/test.png">
-                    </img>
+                    <img class="entrée" src="resources/images/test.png">
                 </a>
             </div>
         </div>
         <div class="bloc2">
             <div class="table">
-                <h1></h1>
-                <span></span>
+                <a href="traitement.php?plat=test2&prix=1.1">
+                    <img class="plat" src="resources/images/test.png">
+                </a>
+            </div>
+        </div>
+        <div class="bloc3">
+            <div class="table">
+                <a href="traitement.php?plat=test3&prix=1.1">
+                    <img class="desert" src="resources/images/test.png">
+                </a>
             </div>
         </div>
     </div>

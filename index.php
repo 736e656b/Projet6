7049d6f3ac3990ca811bd182ae8178a29e7be4e3
the_nt <25741009+the-nt@users.noreply.github.com>
2019-12-13 09:42:43 +0100

    création 1er plat

--- index.php
+++ index.php
@@ -12,22 +12,24 @@
 <div class="header">
     <h1>Nom restaurant</h1>
 </div>
-
-<div class="bloc-table">
-    <div class="bloc1">
-        <div class="table">
-            <h1></h1>
-            <span></span>
+<form action="traitement.php" method="get">
+    <div class="bloc-table">
+        <div class="bloc1">
+            <div class="table">
+                <a href="traitement.php?plat=test1&prix=1.1">
+                    <img class="plat" src="resources/images/test.png">
+                    </img>
+                </a>
+            </div>
+        </div>
+        <div class="bloc2">
+            <div class="table">
+                <h1></h1>
+                <span></span>
+            </div>
         </div>
     </div>
-    <div class="bloc2">
-        <div class="table">
-            <h1></h1>
-            <span></span>
-        </div>
-    </div>
-</div>
-
+</form>
 <i class="far fa-grin-tongue-squint"></i>
 <a class="gotopbtn" href="#"> <i class="fas fa-arrow-up"></i> </a>
 <div class="footer">

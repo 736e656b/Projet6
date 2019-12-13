97dab8825cfce5aa359050a6cf14d2c21b5499bb
the_nt <25741009+the-nt@users.noreply.github.com>
2019-12-13 10:14:45 +0100

    requette debut

--- index.php
+++ index.php
@@ -12,39 +12,39 @@
 <div class="header">
     <h1>Nom restaurant</h1>
 </div>
-<form action="traitement.php" method="get">
+<form action="index.php" method="get">
     <div class="bloc-table">
         <div class="bloc1">
             <div class="table">
-                <a href="traitement.php?plat=test1&prix=1.1">
+                <a href="index.php?plat=test1&prix=1.1">
                     <img class="plat" src="resources/images/test.png">
                 </a>
             </div>
         </div>
         <div class="bloc2">
             <div class="table">
-                <a href="traitement.php?plat=test2&prix=1.1">
+                <a href="index.php?plat=test2&prix=1.1">
                     <img class="plat" src="resources/images/test.png">
                 </a>
             </div>
         </div>
         <div class="bloc3">
             <div class="table">
-                <a href="traitement.php?plat=test3&prix=1.1">
+                <a href="index.php?plat=test3&prix=1.1">
                     <img class="plat" src="resources/images/test.png">
                 </a>
             </div>
         </div>
         <div class="bloc4">
             <div class="table">
-                <a href="traitement.php?plat=test4&prix=1.1">
+                <a href="index.php?plat=test4&prix=1.1">
                     <img class="plat" src="resources/images/test.png">
                 </a>
             </div>
         </div>
         <div class="bloc5">
             <div class="table">
-                <a href="traitement.php?plat=test5&prix=1.1">
+                <a href="index.php?plat=test5&prix=1.1">
                     <img class="plat" src="resources/images/test.png">
                 </a>
             </div>

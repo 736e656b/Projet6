2ca3aad560f35c14c7431c50954d227d984b2751
the_nt <25741009+the-nt@users.noreply.github.com>
2019-12-13 09:15:18 +0100

    debut vrai

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Turret+Road&display=swap" rel="stylesheet">
+    <link href="resources/css/style.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/8fb1e1fba5.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <title>Nom restaurant</title>
+</head>
+<body>
+
+<div class="header">
+    <h1>Nom restaurant</h1>
+</div>
+
+<div class="bloc-table">
+    <div class="bloc1">
+        <div class="table">
+            <h1></h1>
+            <span></span>
+        </div>
+    </div>
+    <div class="bloc2">
+        <div class="table">
+            <h1></h1>
+            <span></span>
+        </div>
+    </div>
+</div>
+
+<i class="far fa-grin-tongue-squint"></i>
+<a class="gotopbtn" href="#"> <i class="fas fa-arrow-up"></i> </a>
+<div class="footer">
+    <a>Crée par</a>
+</div>
+</body>
+</html>

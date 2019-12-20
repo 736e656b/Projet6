940a63b21c0db225aca3301985d91686c9743502
the_nt <25741009+the-nt@users.noreply.github.com>
2019-12-20 10:29:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,50 +10,57 @@
 <body>
 
 <?php
-// Si l'étiquette 'plat' n'existe pas encore dans le tableau associatif $_GET (premier appel au fichier)
 if (!isset($_REQUEST['plat'])) {
     $_REQUEST["plat"]="";
     $_REQUEST["prix"]=0;
-    $total=0;
+    $_REQUEST["total"]=0;
+    $_REQUEST["list"]="";
+    $totalprix=0;
+    $list="";
+}
+else{
+    $totalprix = $_REQUEST["total"];
+    $totalprix = $totalprix + $_REQUEST["prix"];
+    $list = $_REQUEST["list"];
 }
 ?>
 
 <div class="header">
     <h1>Nom restaurant</h1>
 </div>
-<form action="index.php" method="post">
+<form action="index.php" method="get">
     <div class="bloc-table">
         <div class="bloc1">
             <div class="table">
-                <a href="index.php?plat=test1&prix=1.1">
+                <a href="index.php?plat=test1&prix=1.1&total=<?php echo $totalprix; ?>&list=<?php echo $list;?>test1<br>">
                     <img class="plat" src="resources/images/test.png">
                 </a>
             </div>
         </div>
         <div class="bloc2">
             <div class="table">
-                <a href="index.php?plat=test2&prix=1.1">
+                <a href="index.php?plat=test2&prix=1.1&total=<?php echo $totalprix?>&list=<?php echo $list;?>test2<br>">
                     <img class="plat" src="resources/images/test.png">
                 </a>
             </div>
         </div>
         <div class="bloc3">
             <div class="table">
-                <a href="index.php?plat=test3&prix=1.1">
+                <a href="index.php?plat=test3&prix=1.1&total=<?php echo $totalprix?>&list=<?php echo $list;?>test3<br>">
                     <img class="plat" src="resources/images/test.png">
                 </a>
             </div>
         </div>
         <div class="bloc4">
             <div class="table">
-                <a href="index.php?plat=test4&prix=1.1">
+                <a href="index.php?plat=test4&prix=1.1&total=<?php echo $totalprix?>&list=<?php echo $list;?>test4<br>">
                     <img class="plat" src="resources/images/test.png">
                 </a>
             </div>
         </div>
         <div class="bloc5">
             <div class="table">
-                <a href="index.php?plat=test5&prix=1.1">
+                <a href="index.php?plat=test5&prix=1.1&total=<?php echo $totalprix?>&list=<?php echo $list;?>test5<br>">
                     <img class="plat" src="resources/images/test.png">
                 </a>
             </div>
@@ -66,28 +73,7 @@ if (!isset($_REQUEST['plat'])) {
         <div class="table">
             <h1>Liste commande</h1>
             <?php
-            $commande="";
-            $prix="";
-
-            $commande = $_REQUEST['plat'];
-            $prix = $_REQUEST['prix'];
-            $liste_commande_prix= array($commande => $prix );
-            $liste_commande = array_push($liste_commande_prix,$commande,$prix);
-
-            if ($commande != "")
-            {
-
-                //echo "-" . $commande . "    " . $prix;
-                foreach ($test as $commande => $prix)
-                {
-                    echo "l'article " . $commande . " vaut " . $prix . "\n";
-                    echo "<br>";
-                }
-            }
-            else
-            {
-                echo "";
-            }
+            echo $list;
             ?>
         </div>
     </div>
@@ -95,9 +81,9 @@ if (!isset($_REQUEST['plat'])) {
         <div class="table">
             <h2>Total:</h2>
             <?php
-
-            //$total=$total+$prix;
-            echo $total;
+            if ($_REQUEST["plat"] !=""){
+                echo $totalprix;
+            }
             ?>
         </div>
     </div>

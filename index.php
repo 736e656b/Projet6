25799e398784b401365a93c1f76d0dd6b7d3f79b
the_nt <25741009+the-nt@users.noreply.github.com>
2020-01-10 11:06:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,19 +10,42 @@
 <body>
 
 <?php
-if (!isset($_REQUEST['plat'])) {
-    $_REQUEST["plat"]="";
-    $_REQUEST["prix"]=0;
-    $_REQUEST["total"]=0;
-    $_REQUEST["list"]="";
-    $totalprix=0;
-    $list="";
+session_start();
+if (!isset($_SESSION['count'])) {
+    $count= $_SESSION['count'] = 0;
+} else {
+    $count= $_SESSION['count']++;
 }
-else{
-    $totalprix = $_REQUEST["total"];
-    $totalprix = $totalprix + $_REQUEST["prix"];
-    $list = $_REQUEST["list"];
+
+if ($count == 0) {
+    $_SESSION["plat1"]=0;
+    $_SESSION["plat2"]=0;
+    $_SESSION["plat3"]=0;
+    $_SESSION["plat4"]=0;
+    $_SESSION["plat5"]=0;
+    $_SESSION["total"]=0;
 }
+echo $_SESSION["count"];
+
+$plat1_prix = 12;
+$plat2_prix = 8;
+$plat3_prix = 7;
+$plat4_prix = 88;
+$plat5_prix = 1084;
+
+//if (!isset($_REQUEST['plat'])) {
+//    $_REQUEST["plat"]="";
+//    $_REQUEST["prix"]=0;
+//    $_REQUEST["total"]=0;
+//    $_REQUEST["list"]="";
+//    $totalprix=0;
+//    $list="";
+//}
+//else{
+//    $totalprix = $_REQUEST["total"];
+//    $totalprix = $totalprix + $_REQUEST["prix"];
+//    $list = $_REQUEST["list"];
+//}
 ?>
 
 <div class="header">
@@ -32,7 +55,7 @@ else{
     <div class="bloc-table">
         <div class="bloc1">
             <div class="table">
-                <a href="index.php?plat=menu%20varié%20SKY&prix=1.1&total=<?php echo $totalprix; ?>&list=<?php echo $list;?>menu%20varié%20SKY<br>">
+                <a href="index.php?plat=menu%20varié%20SKY&prix=1.1&total=<?php $_SESSION["plat1"] +=1 ;?>menu%20varié%20SKY<br>">
                     <img class="plat" src="resources/images/menu varié SKY.jpg">
                     menu varié SKY
                 </a>
@@ -40,7 +63,7 @@ else{
         </div>
         <div class="bloc2">
             <div class="table">
-                <a href="index.php?plat=menu%20varié%20DUO&prix=1.1&total=<?php echo $totalprix?>&list=<?php echo $list;?>menu%20varié%20DUO<br>">
+                <a href="index.php?plat=menu%20varié%20DUO&prix=1.1&total=<?php $_SESSION["plat2"] +=1 ;?>menu%20varié%20DUO<br>">
                     <img class="plat" src="resources/images/menu varié DUO.jpg">
                     menu varié DUO
                 </a>
@@ -48,7 +71,7 @@ else{
         </div>
         <div class="bloc3">
             <div class="table">
-                <a href="index.php?plat=brochettes%20mix&prix=1.1&total=<?php echo $totalprix?>&list=<?php echo $list;?>brochettes%20mix<br>">
+                <a href="index.php?plat=brochettes%20mix&prix=1.1&total=<?php $_SESSION["plat3"] +=1 ;?>brochettes%20mix<br>">
                     <img class="plat" src="resources/images/brochettes mix.jpg">
                     brochettes mix
                 </a>
@@ -56,7 +79,8 @@ else{
         </div>
         <div class="bloc4">
             <div class="table">
-                <a href="index.php?plat=menu%20Max%20(20%20Pieces)&prix=1.1&total=<?php echo $totalprix?>&list=<?php echo $list;?>menu%20Max%20(20%20Pieces)<br>">
+
+                <a href="index.php?plat=menu%20Max%20(20%20Pieces)&prix=1.1&total=<?php $_SESSION["plat4"] +=1 ;?>menu%20Max%20(20%20Pieces)<br>">
                     <img class="plat" src="resources/images/menu Max (20 Pieces).jpg">
                     menu Max (20 Pieces)
                 </a>
@@ -64,7 +88,7 @@ else{
         </div>
         <div class="bloc5">
             <div class="table">
-                <a href="index.php?plat=menu%20Max%20(40%20Pieces)&prix=1.1&total=<?php echo $totalprix?>&list=<?php echo $list;?>menu%20Max%20(40%20Pieces)<br>">
+                <a href="index.php?plat=menu%20Max%20(40%20Pieces)&prix=1.1&total=<?php $_SESSION["plat5"] +=1 ;?>menu%20Max%20(40%20Pieces)<br>">
                     <img class="plat" src="resources/images/menu Max (40 pieces).jpg">
                     menu Max (40 Pieces)               
                 </a>
@@ -78,7 +102,15 @@ else{
         <div class="table">
             <h1>Liste commande</h1>
             <?php
-            echo $list;
+            if ($count != 0) {
+                echo $_SESSION["plat1"];
+                echo $_SESSION["plat2"];
+                echo $_SESSION["plat3"];
+                echo $_SESSION["plat4"];
+                echo $_SESSION["plat5"];
+            }
+
+
             ?>
         </div>
     </div>
@@ -86,9 +118,7 @@ else{
         <div class="table">
             <h2>Total:</h2>
             <?php
-            if ($_REQUEST["plat"] !=""){
-                echo $totalprix." €";
-            }
+            echo "prix ici";
             ?>
         </div>
     </div>
